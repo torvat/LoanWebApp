20dfe1af38c857cd9bb11db95ff62602ba410261
torvat <torb.vatnelid@gmail.com>
2022-10-24 15:50:57 +0200

    Changes to the test class

--- src/test/java/no/zdata/torva/houseingloanweb/objects/HouseingLoanTest.java
+++ src/test/java/no/zdata/torva/houseingloanweb/objects/HouseingLoanTest.java
@@ -8,38 +8,24 @@ class HouseingLoanTest {
 
     HouseingLoan loan_1 = new HouseingLoan(1_000_000, 10);
     HouseingLoan loan_2 = new HouseingLoan(0, 10);
-    HouseingLoan loan_3 = new HouseingLoan(1_000_000, 0);
-    HouseingLoan loan_4 = new HouseingLoan(10, 1);
-    HouseingLoan loan_5 = new HouseingLoan(1_500_000, 15);
+    HouseingLoan loan_3 = new HouseingLoan(1_000_000, 30);
+    HouseingLoan loan_4 = new HouseingLoan(1_500_000, 15);
 
 
     @Test
     void calculation_1() {
-        assertEquals(8625, loan_1.calculateMonthlyPayment());
+        assertEquals(9889, Math.round(loan_1.calculateMonthlyPayment()));
     }
     @Test
-
     void calculation_2() {
         assertEquals(0, loan_2.calculateMonthlyPayment());
     }
     @Test
     void calculation_3() {
-        Exception exception = assertThrows(RuntimeException.class, () -> loan_3.calculateMonthlyPayment());
-
-        String expectedMessage = "Duration cannot be 0 or less";
-        String actualMessage = exception.getMessage();
-
-        assertTrue(actualMessage.contains(expectedMessage));
+        assertEquals(4490, Math.round(loan_3.calculateMonthlyPayment()));
     }
     @Test
-
     void calculation_4 () {
-        assertEquals(0.8625, loan_4.calculateMonthlyPayment());
-    }
-    @Test
-
-    void calculation_5 () {
-        assertEquals(8625, loan_5.calculateMonthlyPayment());
-
+        assertEquals(10723, Math.round(loan_4.calculateMonthlyPayment()));
     }
 }
